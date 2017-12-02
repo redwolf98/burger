bc16c155d685847287bab1f35510da09e0655340
Anthony Habash <redwolf98@gmail.com>
2017-12-02 12:53:09 -0500

    final

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,31 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Rorick23Raven",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+      port: 3306,
+      host: "localhost",
+      user: "root",
+      password: "Rorick23Raven",
+      database: "burgers_db"
+    });
+}
+
+connection.connect();
 
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// var 
+
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 
 module.exports = connection;
